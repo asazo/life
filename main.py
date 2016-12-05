3aece569e2672e359dc1291b4d907db5b353e107
Alejandro Sazo <alejandrosazog@gmail.com>
2016-12-05 16:54:32 -0300

    Improved slicing over world for get neighbors

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: alejandro
 # @Date:   2016-12-04 15:52:15
-# @Last Modified by:   alejandro
-# @Last Modified time: 2016-12-04 18:59:37
+# @Last Modified by:   Alejandro Sazo
+# @Last Modified time: 2016-12-05 16:53:35
 
 import numpy as np
 import pygame as pg
@@ -19,17 +19,17 @@ class WorldSimulator:
         self.screen_size = screen_size
         self.cell_size = cell_size
         # store current state
-        self.world = np.zeros((screen_size / cell_size)).astype(bool)
+        self.world = np.zeros((screen_size / cell_size))
         # Indices
         self.xx = np.arange(self.world.shape[0])
         self.yy = np.arange(self.world.shape[1])
 
 
-    def activate(self, pos, from_mouse=False):
+    def activate(self, pos):
         """ Activate or deactivate a cell in a position """
-        if from_mouse:
-            pos = np.array(pos) / self.cell_size
+        pos = np.array(pos) / self.cell_size
         self.world[pos[0], pos[1]] = not self.world[pos[0], pos[1]]
+
 
     def draw(self, screen):
         """ """
@@ -39,18 +39,16 @@ class WorldSimulator:
                 color = (255, (1-self.world[i][j])*255, (1-self.world[i][j])*255)
                 pg.draw.rect(screen, color, rect)
 
+
     def sum_over_neighbors(self, i, j):
         """ Sum over neighbors states. """
-        alive = 0
         il = (i-1) % (self.screen_size[0] / self.cell_size)
         ir = (i+1) % (self.screen_size[0] / self.cell_size)
         jl = (j-1) % (self.screen_size[1] / self.cell_size)
         jr = (j+1) % (self.screen_size[1] / self.cell_size)
-        alive = self.world[il][jl].astype(int) + self.world[il][j].astype(int) + \
-                self.world[il][jr].astype(int) + self.world[i][jl].astype(int) + \
-                self.world[i][jr].astype(int) + self.world[ir][jl].astype(int) + \
-                self.world[ir][j].astype(int) + self.world[ir][jr].astype(int)
-        return alive
+        neighbors = self.world[np.ix_([il,i,ir],[jl,j,jr])]
+        return np.sum(neighbors, dtype=np.int32)
+
 
     def update(self):
         # store temp state
@@ -58,19 +56,20 @@ class WorldSimulator:
         for i in self.xx:
             for j in self.yy:
                 alive = self.sum_over_neighbors(i, j)
-                if not self.world[i][j] and alive == 3:
-                    tmp[i][j] = True
-                elif self.world[i][j] and (alive == 3 or alive == 2):
-                    tmp[i][j] = True
+                if self.world[i][j] == 0 and alive == 3:
+                    tmp[i][j] = 1
+                elif self.world[i][j] == 1 and (alive == 3 or alive == 2):
+                    tmp[i][j] = 1
                 else:
-                    tmp[i][j] = False
+                    tmp[i][j] = 0
         self.world = tmp
         del tmp
+
 
 pg.init()
 np.random.seed(35)
 screen_size = np.array([800, 600])
-cell_size = 10
+cell_size = 50
 screen = pg.display.set_mode(screen_size)
 pg.display.set_caption('Life')
 
@@ -86,7 +85,7 @@ while True:
             pg.quit()
             sys.exit()
         if event.type == pg.MOUSEBUTTONUP:
-            w.activate(pg.mouse.get_pos(), from_mouse=True)
+            w.activate(pg.mouse.get_pos())
 
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_e:
